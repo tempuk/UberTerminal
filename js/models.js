b2fe012ba55340fa0aad75aeaf1aba69601458bf
Uzi Kilon <uzi@kilon.org>
2012-02-01 00:47:39 -0800

    re-sorted commands

--- js/models.js
+++ js/models.js
@@ -20,49 +20,50 @@ window.kilon.org.models = kilon.org.models || {};
         this.events = {};
     }
     
-    CommandFactory.add(new Command('date', 'Shows the date', function(){
+    CommandFactory.add(new Command('date', 'show date', function(){
         return [(new Date().toString())];
     }));
-    CommandFactory.add(new Command('exit', 'Quit the terminal', function(){
-        return ["logout", "", "[Process completed]"];
-    }));
-    
+
     CommandFactory.add(new Command('label', null, function(){
         return ["$"];
     }));
     
-    CommandFactory.add(new Command('who', "About me", function(){
+    CommandFactory.add(new Command('who', "about me", function(){
         return [
             'Uzi Kilon',
             'Senior Software Engineer',
-            'Currently work for Splunk as a UI enginner',
+            'Currently working for Splunk as a Core UI enginner',
             '<a href="http://www.linkedin.com/in/uzikilon">http://www.linkedin.com/in/uzikilon</a>',
             '<a href="https://twitter.com/#!/uzikilon">https://twitter.com/#!/uzikilon</a>',
             '<a href="/Uzi.Kilon.pdf">Resume (pdf format)</a>'
         ];
     }));
     
-    CommandFactory.add(new Command('what', "Active Project", function(){
+    CommandFactory.add(new Command('what', "active project", function(){
         return [
-            'Active Projects:',
+            'Current Projects:',
             'UberTerminal (this site) - <a href="http://kilon.org/">http://kilon.org/</a> | <a href="https://github.com/uzikilon/UberTerminal">https://github.com/uzikilon/UberTerminal</a>',
         ];
     }));
-    CommandFactory.add(new Command('welcome', "Welcome Message", function(){
+    CommandFactory.add(new Command('welcome', "welcome message", function(){
         return [
             'UberTerminal 0.1 By Uzi Kilon &copy;2012',
             'Type "help" for available commands',
         ];
     }));
     
-    CommandFactory.add(new Command('clear', 'Clear window state'));
+    CommandFactory.add(new Command('clear', 'clear window'));
     
-    CommandFactory.add(new Command('help', 'Show this message', function(){
-        var s = ["Available Commands:"];
+    CommandFactory.add(new Command('exit', 'exit the terminal', function(){
+        return ["logout", "", "[Process completed]"];
+    }));
+    
+    CommandFactory.add(new Command('help', 'show this message', function(){
+        var s = ["<u>available commands:</u>"];
         for (var name in this._commands) {
             var command = this._commands[name];
             if (command.help) {
-                s.push(command.name + "\t" + command.help);
+                s.push(command.name + "\t\t" + command.help);
             }
         }
         return s;
