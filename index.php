aa1765ec3e5851dfdd8884dcfebbdf6f896ff59e
Uzi Kilon <uzi@kilon.org>
2012-02-01 01:01:23 -0800

    HTML Cleanup

--- index.php
+++ index.php
@@ -5,22 +5,18 @@
 <title>Uzi Kilon</title>
 </head>
 <body>
-
     <header>
   __  __     _    __ ___ __         
  / / / /___ (_)  / //_(_) /___  ___ 
 / /_/ //_ // /  / ,< / / // _ \/ _ \
 \____/ /__/_/  /_/|_/_/_/ \___/_//_/
      </header>
-    
     <br>
-    
     <div id="terminal"></div>
     <form onsubmit="return false">
         <label for="q">$</label>
         <input type="text" style="width: 90%" name="q" id="q" autocomplete="off">
     </form>
-
     <script src="/js/jquery-1.7.1.min.js"></script>
     <script src="/js/jquery.hotkeys.js"></script>
     <script src="/js/underscore.js"></script>
@@ -28,6 +24,5 @@
     <script src="/js/models.js"></script>
     <script src="/js/views.js"></script>
     <script src="/js/app.js"></script>
-    
 </body>
 </html>
